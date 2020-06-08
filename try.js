471f2270138bcf1c2d135de631d5cddd7421da5d
toastal <toastal@protonmail.com>
2020-06-08 13:44:08 +0700

    drop sync-exec for security, upgrade sleep, normalize scope to main

--- try.js
+++ try.js
@@ -1,38 +1,33 @@
 #!/usr/bin/env node
-var commander = require('commander')
-var exec = require('sync-exec');
-var sleep = require('sleep');
-var pack = require('./package.json');
 
-var attempts,
-    timeout,
-    command,
-    attemptCount = 0
+var commander = require('commander')
+var execSync = require('child_process').execSync
+var sleep = require('sleep')
+var pack = require('./package.json')
 
 commander
     .version(pack.version)
     .description(pack.description)
+    .option('-c, --command  <cmd>', 'Command to try')
     .option('-a, --attempts <number>', 'Maximum number of retry attempts. DEFAULT = 4')
     .option('-t, --timeout  <seconds>', 'Seconds between retries. DEFAULT = 4')
-    .option('-c, --command  <cmd>', 'Command to try')
     .option('-f, --force-repeat', 'Force repeated execution even if it succeeds')
     .parse(process.argv)
 
 function main() {
-    setArgs()
+    var command = commander.command || ''
+    var attemptCount = 0
+    var attempts = parseInt(commander.attempts || 4, 10)
+    var timeout = parseInt(commander.timeout || 4, 10)
+    var forceRepeat = commander.forceRepeat || false
+
     while (attemptCount++ < attempts) {
-        var res = exec(command, { stdio: [0, 1, 2] })
-        if (res.status == 0 && !commander.forceRepeat)
+        var res = execSync(command, { stdio: [0, 1, 2] })
+        if (res.status == 0 && !forceRepeat)
             break
-        sleep.sleep(parseInt(timeout))
+        sleep.sleep(timeout)
     }
 }
 
-function setArgs() {
-    timeout = commander.timeout || 4
-    attempts = commander.attempts || 4
-    command = commander.command || ''
-}
-
 main()
-process.on('SIGINT', process.exit());
+process.on('SIGINT', process.exit())
